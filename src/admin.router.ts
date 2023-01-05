52d8c71d6c89fc847f1277c81be3d3465781a0e1
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-05 23:47:36 +0530

    admin bro secure

--- src/admin.router.ts
+++ src/admin.router.ts
@@ -5,30 +5,30 @@ import bcrypt from "bcrypt";
 const cookiePass = process.env.ADMIN_SECRET || "secret";
 
 const buildAdminRouter = (admin) => {
-  const router = AdminBroExpress.buildRouter(admin);
-  // const router = AdminBroExpress.buildAuthenticatedRouter(
-  //   admin,
-  //   {
-  //     authenticate: async (email, password) => {
-  //       const user = await AdminBroUser.findOne({ email });
-  //       if (user) {
-  //         // const pass = await bcrypt.compare(password, user.encryptedPassword);
-  //         const pass = password === user.encryptedPassword;
-  //         if (pass) {
-  //           return user;
-  //         }
-  //       }
-  //       return false;
-  //     },
-  //     cookieName: "GSM",
-  //     cookiePassword: cookiePass,
-  //   },
-  //   null,
-  //   {
-  //     resave: true,
-  //     saveUninitialized: true,
-  //   }
-  // );
+  // const router = AdminBroExpress.buildRouter(admin);
+  const router = AdminBroExpress.buildAuthenticatedRouter(
+    admin,
+    {
+      authenticate: async (email, password) => {
+        const user = await AdminBroUser.findOne({ email });
+        if (user) {
+          const pass = await bcrypt.compare(password, user.encryptedPassword);
+          // const pass = password === user.encryptedPassword;
+          if (pass) {
+            return user;
+          }
+        }
+        return false;
+      },
+      cookieName: "USIP",
+      cookiePassword: cookiePass,
+    },
+    null,
+    {
+      resave: true,
+      saveUninitialized: true,
+    }
+  );
   return router;
 };
 
