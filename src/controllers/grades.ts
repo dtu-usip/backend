0b20695ed92f4b88cc733c896114d8899c757a0c
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-05 23:07:19 +0530

    Grade controllers

--- src/controllers/grades.ts
+++ src/controllers/grades.ts
@@ -1,54 +1,134 @@
 import { NextFunction, Request, Response } from "express";
+import Enrollment, { EnrollmentType } from "../models/enrollment";
 import ExpressError from "../utils/ExpressError";
 
-export const viewGrades = (req: Request, res: Response, next: NextFunction) => {
-  try {
-  } catch (e) {
-    console.log("There was an error>> ", e);
-    next(new ExpressError("There was an error", 500, e));
-  }
-};
-
-export const totalNumberOfGrades = (
+export const viewGrades = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const { id } = req.user;
+
+    const enrollments: EnrollmentType[] = await Enrollment.find({
+      user_id: id,
+    }).populate("user", "course");
+
+    res.status(200).json({
+      enrollments,
+    });
   } catch (e) {
     console.log("There was an error>> ", e);
     next(new ExpressError("There was an error", 500, e));
+    return;
   }
 };
 
-export const addGrade = (req: Request, res: Response, next: NextFunction) => {
+// Idk the meaning of this
+// I'm just sending enrollments again
+// Can be handled on the frontend ig
+export const totalNumberOfGrades = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const { id } = req.user;
+
+  const enrollments: EnrollmentType[] = await Enrollment.find({
+    user_id: id,
+  }).populate("user", "course");
+
+  res.status(200).json({
+    enrollments,
+  });
   try {
   } catch (e) {
     console.log("There was an error>> ", e);
     next(new ExpressError("There was an error", 500, e));
+    return;
   }
 };
 
-export const updateGrade = (
+export const addGrade = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const { course_id, user_id, mte_score, ete_score } = req.body;
+
+    const enrollment: EnrollmentType = await Enrollment.findOne({
+      course_id,
+      user_id,
+    });
+
+    enrollment.mte_score = mte_score;
+    enrollment.ete_score = ete_score;
+
+    await enrollment.save();
+
+    res.status(200).json({
+      enrollment,
+    });
   } catch (e) {
     console.log("There was an error>> ", e);
     next(new ExpressError("There was an error", 500, e));
+    return;
   }
 };
 
-export const removeGrade = (
+export const updateGrade = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
   try {
+    const { course_id, user_id, mte_score, ete_score } = req.body;
+
+    const enrollment: EnrollmentType = await Enrollment.findOne({
+      course_id,
+      user_id,
+    });
+
+    enrollment.mte_score = mte_score;
+    enrollment.ete_score = ete_score;
+
+    await enrollment.save();
+
+    res.status(200).json({
+      enrollment,
+    });
   } catch (e) {
     console.log("There was an error>> ", e);
     next(new ExpressError("There was an error", 500, e));
+    return;
+  }
+};
+
+export const removeGrade = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const { course_id, user_id, mte_score, ete_score } = req.body;
+
+    const enrollment: EnrollmentType = await Enrollment.findOne({
+      course_id,
+      user_id,
+    });
+
+    enrollment.mte_score = null;
+    enrollment.ete_score = null;
+
+    await enrollment.save();
+
+    res.status(200).json({
+      enrollment,
+    });
+  } catch (e) {
+    console.log("There was an error>> ", e);
+    next(new ExpressError("There was an error", 500, e));
+    return;
   }
 };
