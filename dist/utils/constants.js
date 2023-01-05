09b651ce9cbc8f31f47f8c1caf8dc22d85edab30
Jayesh Sadhwani <jayeshsadhwani99@gmail.com>
2023-01-05 23:39:30 +0530

    removed dist

--- dist/utils/constants.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.REFRESH_TOKEN_EXPIRATION = exports.ACCESS_TOKEN_EXPIRATION = exports.REFRESH_TOKEN = exports.ACCESS_TOKEN = void 0;
-exports.ACCESS_TOKEN = "access";
-exports.REFRESH_TOKEN = "refresh";
-exports.ACCESS_TOKEN_EXPIRATION = 3600 * 60 * 60; // 1 hour
-exports.REFRESH_TOKEN_EXPIRATION = 3600 * 60 * 60 * 24 * 30; // 30 days
-//# sourceMappingURL=constants.js.map
